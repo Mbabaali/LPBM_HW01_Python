3365950e7b4933450a1e574e802ea0f4362fdef7
mbabaali <madzi2122@gmail.com>
2020-02-07 17:35:42 +0100

    Mise a jour de GenGraph

--- interfacebancdetest_dev_6_DUT_To3/genGraph.py
+++ interfacebancdetest_dev_6_DUT_To3/genGraph.py
@@ -1,67 +1,75 @@
 # -*- coding: utf8 -*-
+"""
+    module permetttant la genration automatisé de graph dans le cadre du LPBM
+"""
 from kivy.logger import Logger
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import csv
-import seaborn as sns
 from datetime import datetime
 import sys
 import pandas as pd
 
-def generer_graph(chemin, chemin_graph='', y_low_min=0.02, y_low_max=0.12, y_high_min=600, y_high_max=1000):
+plt.rcParams["figure.figsize"] = (70,12)
+
+def generer_graph(chemin, chemin_graph='',
+                  y_low_min=0.02, y_low_max=0.12,
+                  y_high_min=600, y_high_max=1000, format_graph=0):
+    """
+        Permet de generer un graph diviser en deux suivant les paramètres données
+    """
+
+    if format_graph == 0 :
+        plt.rcParams["figure.figsize"] = (50,12)
+    elif format_graph == 1 :
+        plt.rcParams["figure.figsize"] = (80,12)
+    else :
+        plt.rcParams["figure.figsize"] = (300,12)
+
+    #On règle la dimension du graph en fonction du paramètre "format_graph" reçu (0 par défaut)
+
+
+
     Logger.warning('generer graph: chemin : {}'.format(chemin))
-    if(chemin_graph==""):
-        nom_graph=chemin[:-3:]+'png'
+    if chemin_graph == "":
+        nom_graph = chemin[:-3:]+'png'
+        #Si le chemin n'est pas précisé, on prend le meme nom que le fichier .csv associé (on remplace le .csv par un .png)
     else:
-        nom_graph=chemin_graph+".png"
+        nom_graph = chemin_graph+".png"
     Logger.warning('generer_graph: nom_graph : {}'.format(nom_graph))
     #sns.set_style("darkgrid")
 
-    df=pd.read_csv(chemin, sep=';', decimal=',')
+    dataframe = pd.read_csv(chemin, sep=';') 
 
+    ordonnee = dataframe['Value']
 
-    y=df['Value']
+    abcisse = dataframe['date-time']
+    abcisse = pd.to_datetime(dataframe['date-time'], 
+                             format='%Y-%m-%d %H:%M:%S.%f').astype(datetime)
+    try:
+        plt.plot(abcisse, ordonnee, marker=',')
+    except AttributeError:
+        Logger.warning('plt plot: impossible de generer un graph, fichier vide')
+        exit()
+    
 
-    x=df['date-time']
-    x=pd.to_datetime(df['date-time'], format='%Y-%m-%d %H:%M:%S.%f').astype(datetime)
-
-    # y_low_min=y.min()-0.001
-    # # y_low_max=y.min()+0.1
-
-    # y_high_max=y.max()+(y.max()*10/100)
-    # y_high_min=y.max()-(y.max()*35/100)
-    # print(y.min())
-    # print(y.max())
-    # print(y_low_min)
-    # print(y_low_max)
-    # print(y_high_min)
-    # print(y_high_max)
-    # print(y.idxmin())
-
-    # x=[]
-    # y=[]
-
-    # i=1
-
-    # with open(chemin, 'r') as csvfile:
-    #     plots= csv.reader(csvfile, delimiter=';')
-    #     next(plots)
-    #     for row in plots:
-    #         y.append(float((row[1]).replace(',','.')))
-    #         date=datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
-    #         x.append(date)
-
-
-    plt.plot(x,y, marker=',')
-
-    f, (ax, ax2) = plt.subplots(2, 1, sharex=True)
-    ax.plot(x,y,  linewidth=1)
-    ax2.plot(x,y,  linewidth=1)
+    func, (ax, ax2) = plt.subplots(2, 1, sharex=True)
+    ax.plot(abcisse, ordonnee, linewidth=1) #graphe du haut
+    ax2.plot(abcisse, ordonnee, linewidth=1) #graphe du bas
 
     # zoom-in / limit the view to different portions of the data
     ax.set_ylim(float(y_high_min), float(y_high_max))  # outliers only
     ax2.set_ylim(float(y_low_min), float(y_low_max))  # most of the data
+
+    # rotate and align the tick labels so they look better
+    func.autofmt_xdate()
+
+
+    ax2.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(interval=60))
+
+    # use a more precise date string for the x axis locations in the
+    # toolbar
+    ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%d/%m  %H:%M'))
 
     # hide the spines between ax and ax2
     ax.spines['bottom'].set_visible(False)
@@ -70,42 +78,38 @@ def generer_graph(chemin, chemin_graph='', y_low_min=0.02, y_low_max=0.12, y_hig
     ax.tick_params(labeltop='off')  # don't put tick labels at the top
     ax2.xaxis.tick_bottom()
 
-    d = .015  # how big to make the diagonal lines in axes coordinates
+    diag = .015  # how big to make the diagonal lines in axes coordinates
     # arguments to pass to plot, just so we don't keep repeating them
     kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
-    ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
-    ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
-
+    ax.plot((-diag, +diag), (-diag, +diag), **kwargs)        # top-left diagonal
+    ax.plot((1 - diag, 1 + diag), (-diag, +diag), **kwargs)  # top-right diagonal
     kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
-    ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
-    ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
-
-
-    f.suptitle('Data from the CSV File: conso en mA')
-
+    ax2.plot((-diag, +diag), (1 - diag, 1 + diag), **kwargs)  # bottom-left diagonal
+    ax2.plot((1 - diag, 1 + diag), (1 - diag, 1 + diag), **kwargs)  # bottom-right diagonal
+    func.suptitle(str(chemin))
     plt.xlabel('Date')
     plt.ylabel('Conso (en mA)')
-
     plt.savefig(nom_graph)
 
+if __name__ == "__main__":
     
-    
-
-if __name__ == "__main__":    
-    
-    if((len(sys.argv)!=2) and (len(sys.argv)!=3) and (len(sys.argv)!=7)):
-        Logger.warning('ERROR: Nombre d\'argument invalide : nombre d\'argument : {}'.format(len(sys.argv)) )
-    
-    elif(len(sys.argv)==2):    
-        chemin=sys.argv[1]
-        generer_graph(chemin)
-    
-    elif(len(sys.argv)==3):
-        chemin=sys.argv[1]
-        chemin_graph=sys.argv[2]
-        generer_graph(chemin, chemin_graph)
-    elif(len(sys.argv)==7):
-        generer_graph(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+    """
+    Fonction main, pour lancer le script gengraph directement depuis le terminal
+    """
+    if(len(sys.argv) != 2) and (len(sys.argv) != 3) and (len(sys.argv) != 7):
+        Logger.warning(
+        'ERROR: Nombre d\'argument invalide : nombre d\'argument : {}'
+        .format(len(sys.argv))) 
+    elif len(sys.argv) == 2:    
+        CHEMIN = sys.argv[1]
+        generer_graph(CHEMIN)  
+    elif len(sys.argv) == 3:
+        CHEMIN = sys.argv[1]
+        CHEMIN_GRAPH = sys.argv[2]
+        generer_graph(CHEMIN, CHEMIN_GRAPH)
+    elif len(sys.argv) == 7:
+        generer_graph(sys.argv[1], sys.argv[2], sys.argv[3], 
+                      sys.argv[4], sys.argv[5], sys.argv[6])
 
 
 
