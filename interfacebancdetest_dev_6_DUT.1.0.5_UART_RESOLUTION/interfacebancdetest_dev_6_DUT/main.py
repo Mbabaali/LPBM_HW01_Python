2cb0cbe17bb8db6d3d9e1bc9c0be4a936d35eb3d
mbabaali <madzi2122@gmail.com>
2020-02-11 15:45:31 +0100

    réglages de cpt error

--- interfacebancdetest_dev_6_DUT.1.0.5_UART_RESOLUTION/interfacebancdetest_dev_6_DUT/main.py
+++ interfacebancdetest_dev_6_DUT.1.0.5_UART_RESOLUTION/interfacebancdetest_dev_6_DUT/main.py
@@ -1449,7 +1449,7 @@ class Voie1234(Screen):
         Logger.warning('trame envoyé: {}'.format(trame_bus))
 
         #Logger.warning('test : ligne n°{}: Vérif '.format(inspect.currentframe().f_lineno))
-
+        cpt_erreur=0
         ok=False
         msg="rien reçu"
         while(ok==False):
