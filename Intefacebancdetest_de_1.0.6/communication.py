f26ac7bd419d1ba95646763665eef7b2b2793951
mbabaali <madzi2122@gmail.com>
2020-02-19 14:11:29 +0100

    Mise à jour du code

---
+++ Intefacebancdetest_de_1.0.6/communication.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+
+import serial
+
+if __name__ == "__main__":
+    try:
+        arduino1 = serial.Serial('/dev/ttyACM0',115200,timeout = 10)
+        
+
+    except:
+        print("Failed to connect à l'arduino")
