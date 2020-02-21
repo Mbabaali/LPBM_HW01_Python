cff50d19580e48c7766d8f571aec1befcd79025a
mbabaali <madzi2122@gmail.com>
2020-02-21 10:50:26 +0100

    print('type(U1: ')

--- Intefacebancdetest_de_1.0.6/main.py
+++ Intefacebancdetest_de_1.0.6/main.py
@@ -1103,7 +1103,11 @@ class Voie1234(Screen):
                 # U4 = int(U4)
                 # U5 = int(U5)
                 # U6 = int(U6)
+                print('type(passerelle.U1onMax): ')
                 print(type(passerelle.U1onMax))
+
+                print('type(U1: ')
+                print(type(U1))
                 if(passerelle.U1onMax<U1):
                     passerelle.U1onMax=U1
                     passerelle.A1onMax=A1
