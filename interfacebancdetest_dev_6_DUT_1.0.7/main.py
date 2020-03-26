656c7345597606b71609dc655ad0a5f264ed9ac6
mbabaali <madzi2122@gmail.com>
2020-03-26 11:37:28 +0100

    suppresion des labels des puissances dans l'interface

--- interfacebancdetest_dev_6_DUT_1.0.7/main.py
+++ interfacebancdetest_dev_6_DUT_1.0.7/main.py
@@ -297,38 +297,38 @@ class Voie1234(Screen):
         # Labels courant et conso Dut1
         self.courant1 = Label(text="courant1", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.24, 'center_y':0.88})
         self.add_widget(self.courant1)
-        self.puissance1 = Label(text="puissance1", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.24, 'center_y':0.80})
-        self.add_widget(self.puissance1)
+        # self.puissance1 = Label(text="puissance1", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.24, 'center_y':0.80})
+        # self.add_widget(self.puissance1)
         
         # Labels courant et conso Dut2
         self.courant2 = Label(text="courant2", font_size='30sp', size=(100, 50),  pos_hint={'center_x': 0.57, 'center_y':0.88})
         self.add_widget(self.courant2)
-        self.puissance2 = Label(text="puissance2", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.57, 'center_y':0.80})
-        self.add_widget(self.puissance2)
+        # self.puissance2 = Label(text="puissance2", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.57, 'center_y':0.80})
+        # self.add_widget(self.puissance2)
 
         # Labels courant et conso Dut3
         self.courant3 = Label(text="courant3", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.9, 'center_y':0.88})
         self.add_widget(self.courant3)
-        self.puissance3 = Label(text="puissance3", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.9, 'center_y':0.80})
-        self.add_widget(self.puissance3)
+        # self.puissance3 = Label(text="puissance3", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.9, 'center_y':0.80})
+        # self.add_widget(self.puissance3)
 
         # Labels courant et conso Dut4
         self.courant4 = Label(text="courant4", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.24, 'center_y':0.43})
-        self.puissance4 = Label(text="puissance4", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.24, 'center_y':0.35})
         self.add_widget(self.courant4)
-        self.add_widget(self.puissance4)
+        # self.puissance4 = Label(text="puissance4", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.24, 'center_y':0.35})
+        # self.add_widget(self.puissance4)
 
         # Labels courant et conso Dut5
         self.courant5 = Label(text="courant5", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.57, 'center_y':0.43})
-        self.puissance5 = Label(text="puissance5", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.57, 'center_y':0.35})
         self.add_widget(self.courant5)
-        self.add_widget(self.puissance5)
+        # self.puissance5 = Label(text="puissance5", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.57, 'center_y':0.35})
+        # self.add_widget(self.puissance5)
 
         # Labels courant et conso Dut6
         self.courant6 = Label(text="courant6", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.9, 'center_y':0.43})
-        self.puissance6 = Label(text="puissance6", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.9, 'center_y':0.35})
         self.add_widget(self.courant6)
-        self.add_widget(self.puissance6)
+        # self.puissance6 = Label(text="puissance6", font_size='30sp', size=(100, 50), pos_hint={'center_x': 0.9, 'center_y':0.35})
+        # self.add_widget(self.puissance6)
   
         
         
