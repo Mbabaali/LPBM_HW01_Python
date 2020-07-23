f80bf3b07adf735b81fac25a95cad524b867f2a0
mbabaali <madzi2122@gmail.com>
2020-07-23 09:59:38 +0200

    Un putain de commit pour le graph

--- interfacebancdetest_dev_6_DUT_1.0.9/GenGraph.py
+++ interfacebancdetest_dev_6_DUT_1.0.9/GenGraph.py
@@ -110,3 +110,11 @@ if __name__ == "__main__":
     elif len(sys.argv) == 7:
         generer_graph(sys.argv[1], sys.argv[2], sys.argv[3], 
                       sys.argv[4], sys.argv[5], sys.argv[6])
+
+
+
+
+
+
+
+    
