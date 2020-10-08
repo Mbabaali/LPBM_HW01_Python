578f7a47f59fb2ba78a1ae4cacf7847494484c8f
mbabaali <madzi2122@gmail.com>
2020-10-08 09:51:18 +0200

    Update main.py

--- interfacebancdetest_dev_6_DUT_1.0.11_Enregistrement_Cycles/kyboard/main.py
+++ interfacebancdetest_dev_6_DUT_1.0.11_Enregistrement_Cycles/kyboard/main.py
@@ -6,34 +6,17 @@ import kivy
 # base Class of your App inherits from the App class.     
 # app:always refers to the instance of your application    
 from kivy.app import App  
-     
-# this restrict the kivy version i.e   
-# below this kivy version you cannot   
-# use the app or software   
+
 kivy.require('1.9.0')  
-    
-# The Label widget is for rendering text.   
+ 
 from kivy.uix.label import Label  
     
-# module consist the floatlayout   
-# to work with FloatLayout first   
-# you have to import it   
+
 from kivy.uix.floatlayout import FloatLayout  
   
-# Scatter is used to build interactive 
-# widgets that can be translated, 
-# rotated and scaled with two or more 
-# fingers on a multitouch system. 
 from kivy.uix.scatter import Scatter 
-  
-# The TextInput widget provides a 
-# box for editable plain text 
 from kivy.uix.textinput import TextInput 
   
-# BoxLayout arranges widgets in either 
-# in vertical fashion that 
-# is one on top of another or in 
-# horizontal fashion that is one after another. 
 from kivy.uix.boxlayout import BoxLayout 
 from kivy.config import Config
 
