1aa75aa015b243cb6f8841395fdbfc5b1e523d95
mbabaali <madzi2122@gmail.com>
2020-10-15 09:52:55 +0200

    oublie de l'import de config

--- interfacebancdetest_dev_6_DUT_1.0.11_Enregistrement_Cycles/kyboard/main.py
+++ interfacebancdetest_dev_6_DUT_1.0.11_Enregistrement_Cycles/kyboard/main.py
@@ -4,16 +4,14 @@ from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 import os
 
-from connected import Connected
 
 Config.set('kivy', 'keyboard_mode', 'systemandmulti')
 
 class Login(Screen):
-    def do_login(self, loginText, passwordText):
+    def do_login(self, loginText):
         app = App.get_running_app()
 
         app.username = loginText
-        app.password = passwordText
 
         self.manager.transition = SlideTransition(direction="left")
         self.manager.current = 'connected'
@@ -23,17 +21,14 @@ class Login(Screen):
 
     def resetForm(self):
         self.ids['login'].text = ""
-        self.ids['password'].text = ""
 
 class LoginApp(App):
     username = StringProperty(None)
-    password = StringProperty(None)
 
     def build(self):
         manager = ScreenManager()
 
         manager.add_widget(Login(name='login'))
-        manager.add_widget(Connected(name='connected'))
 
         return manager
 
